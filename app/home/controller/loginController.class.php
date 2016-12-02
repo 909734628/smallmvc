9b5b6eb94d3c23426a7f3fa7f43e03f9275be9cf
XuZhiwei <jsrdxzw@163.com>
2016-12-02 23:28:53 +0800

    fix bugs

---
+++ app/home/controller/loginController.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: myPersonalFile
+ * Date: 2016/12/2
+ * Time: 下午11:26
+ */
+class loginController extends controller
+{
+  public function indexAction(){ //在这里作默认的登录首页
+
+  }
+}
